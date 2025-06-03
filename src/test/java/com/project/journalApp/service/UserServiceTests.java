34dec2484e581588d0f248143bbfbf429509a5fa
Rithik Kumar Singh <rksbugatti@gmal.com>
2025-06-03 23:58:44 +0530

    testing

--- src/test/java/com/project/journalApp/service/UserServiceTests.java
+++ src/test/java/com/project/journalApp/service/UserServiceTests.java
@@ -2,8 +2,7 @@ package com.project.journalApp.service;
 
 import com.project.journalApp.entity.User;
 import com.project.journalApp.repository.UserRepository;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -56,3 +55,6 @@ public class UserServiceTests {
     }
 
 }
+
+//there are other annotations like:
+//@BeforeAll , @BeforeEach, @AfterAll, AfterEach
